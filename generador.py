faab49f19f2aefb860e0de7f9dc19d0b11582a50
Mil-54 <mildredcercru04@gmail.com>
2025-10-10 21:16:32 -0600

    Feat: Creacion inicial del generador de saludos en Python

---
+++ generador.py
@@ -0,0 +1,11 @@
+# generador.py
+
+def saludar(nombre):
+  """Esta función genera un saludo personalizado."""
+  print(f"¡Hola, {nombre}! Bienvenido al mundo de Git y Python.")
+
+# Le pedimos al usuario que ingrese un nombre
+nombre_usuario = input("Por favor, introduce tu nombre: ")
+
+# Llamamos a la función para mostrar el saludo
+saludar(nombre_usuario)
