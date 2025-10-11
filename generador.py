f4f02a7e97a1258011e27efce5d4d9914a4be616
Mil-54 <mildredcercru04@gmail.com>
2025-10-10 21:19:46 -0600

    Añadir saludos aleatorios para mas variedad

--- generador.py
+++ generador.py
@@ -1,8 +1,15 @@
 # generador.py
+import random # Importamos la librería para elegir al azar
 
 def saludar(nombre):
-  """Esta función genera un saludo personalizado."""
-  print(f"¡Hola, {nombre}! Bienvenido al mundo de Git y Python.")
+  """Esta función genera un saludo personalizado y aleatorio."""
+  saludos_posibles = [
+      f"¡Qué bueno verte, {nombre}!",
+      f"¡Hola, {nombre}! Bienvenido al mundo de Git y Python.",
+      f"¡Un saludo, {nombre}! Programar es un placer."
+  ]
+  saludo_elegido = random.choice(saludos_posibles)
+  print(saludo_elegido)
 
 # Le pedimos al usuario que ingrese un nombre
 nombre_usuario = input("Por favor, introduce tu nombre: ")
